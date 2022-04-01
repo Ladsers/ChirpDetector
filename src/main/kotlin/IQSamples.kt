c54a6071f2ea8ac5bd276aa0b60ff4546d7a2303
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2022-04-01 16:03:05 +0300

    Improved class "IQSamples".

--- src/main/kotlin/IQSamples.kt
+++ src/main/kotlin/IQSamples.kt
@@ -1,7 +1,30 @@
+import kotlin.math.atan2
+
 class IQSamples(val i: FloatArray, val q: FloatArray) {
     fun getIQ(index: Int) = Pair(i[index], q[index])
     fun getArrays() = Pair(i, q)
 
     val size = i.size
     val lastIndex = i.lastIndex
+
+    fun getPhase(): FloatArray {
+        val debugUtilities = DebugUtilities("IQSamples.getPhase")
+        debugUtilities.timeStart()
+
+        val phase = FloatArray(size)
+        for (k in 0..lastIndex) phase[k] = atan2(i[k], q[k])
+
+        debugUtilities.timeStop()
+        return phase
+    }
+
+    fun removeI() {
+        for (k in 0..lastIndex) i[k] = 0f
+    }
+
+    fun removeQ() {
+        for (k in 0..lastIndex) q[k] = 0f
+    }
+
+    fun clone() = IQSamples(i.clone(), q.clone())
 }
