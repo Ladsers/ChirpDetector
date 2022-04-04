3b4a733e9d234bdc528e2c40582b7295984c3c35
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2022-04-04 14:21:27 +0300

    Implemented different type of counter for phase.

--- src/main/kotlin/PhaseCounter.kt
+++ src/main/kotlin/PhaseCounter.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 object PhaseCounter {
     fun calcAboveBelow(phase: FloatArray): IntArray {
         val debugUtilities = DebugUtilities("PhaseCounter.calcAboveBelow")
@@ -15,10 +17,42 @@ object PhaseCounter {
                 counter = 1
             }
 
-            if (i == phase.lastIndex)  list.add(counter)
+            if (i == phase.lastIndex) list.add(counter)
         }
 
         debugUtilities.timeStop()
         return list.toIntArray()
+    }
+
+    fun calcEqual(phase: FloatArray, error: Float = 0F): IntArray {
+        val debugUtilities = DebugUtilities("PhaseCounter.calcEqual")
+        debugUtilities.timeStart()
+
+        val list = mutableListOf<Int>()
+        var counter = 1
+
+        for (i in 1..phase.lastIndex) {
+            if (abs(phase[i - 1] - phase[i]) <= error) counter++
+            else {
+                list.add(counter)
+                counter = 1
+            }
+
+            if (i == phase.lastIndex) list.add(counter)
+        }
+
+        debugUtilities.timeStop()
+        return list.toIntArray()
+    }
+
+    fun cutOffTop(data: IntArray, threshold: Int): IntArray {
+        val debugUtilities = DebugUtilities("PhaseCounter.cutOffTop")
+        debugUtilities.timeStart()
+
+        val res = data.clone()
+        for (i in 0..res.lastIndex) if (res[i] > threshold) res[i] = 0
+
+        debugUtilities.timeStop()
+        return res
     }
 }
